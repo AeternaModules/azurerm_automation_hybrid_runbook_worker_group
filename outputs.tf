76d327fef4f849ff7fd2ce99a0c0502d2241593a
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "automation_hybrid_runbook_worker_groups" {
   description = "All automation_hybrid_runbook_worker_group resources"
   value       = azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups
 }
+output "automation_hybrid_runbook_worker_groups_automation_account_name" {
+  description = "List of automation_account_name values across all automation_hybrid_runbook_worker_groups"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.automation_account_name]
+}
+output "automation_hybrid_runbook_worker_groups_credential_name" {
+  description = "List of credential_name values across all automation_hybrid_runbook_worker_groups"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.credential_name]
+}
+output "automation_hybrid_runbook_worker_groups_name" {
+  description = "List of name values across all automation_hybrid_runbook_worker_groups"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.name]
+}
+output "automation_hybrid_runbook_worker_groups_resource_group_name" {
+  description = "List of resource_group_name values across all automation_hybrid_runbook_worker_groups"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.resource_group_name]
+}
 
