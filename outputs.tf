d6dbebd6d29385d688ab6ea6a189e14fab2d5b6f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "automation_hybrid_runbook_worker_groups" {
-  description = "All automation_hybrid_runbook_worker_group resources"
-  value       = azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups
-}
 output "automation_hybrid_runbook_worker_groups_automation_account_name" {
-  description = "List of automation_account_name values across all automation_hybrid_runbook_worker_groups"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.automation_account_name }
 }
 output "automation_hybrid_runbook_worker_groups_credential_name" {
-  description = "List of credential_name values across all automation_hybrid_runbook_worker_groups"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.credential_name]
+  description = "Map of credential_name values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.credential_name }
 }
 output "automation_hybrid_runbook_worker_groups_name" {
-  description = "List of name values across all automation_hybrid_runbook_worker_groups"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.name]
+  description = "Map of name values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.name }
 }
 output "automation_hybrid_runbook_worker_groups_resource_group_name" {
-  description = "List of resource_group_name values across all automation_hybrid_runbook_worker_groups"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.resource_group_name }
 }
 
