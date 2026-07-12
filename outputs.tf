765835c7254c3db2fb88f17899ea0b4bc3337b12
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "automation_hybrid_runbook_worker_groups_id" {
+  description = "Map of id values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.id }
+}
 output "automation_hybrid_runbook_worker_groups_automation_account_name" {
   description = "Map of automation_account_name values across all automation_hybrid_runbook_worker_groups, keyed the same as var.automation_hybrid_runbook_worker_groups"
   value       = { for k, v in azurerm_automation_hybrid_runbook_worker_group.automation_hybrid_runbook_worker_groups : k => v.automation_account_name }
